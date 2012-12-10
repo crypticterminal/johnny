0a42ab59ba4e0e10367ac45ef23d3df4433af6a9
Devon Kearns <dookie@kali.org>
2012-12-09 18:55:36 -0700

    Edited debian files

--- debian/johnny.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the johnny package
-#
-0 4	* * *	root	[ -x /usr/bin/johnny_maintenance ] && /usr/bin/johnny_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(johnny):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="johnny" command="/usr/bin/johnny"
